794ff1ef8779a663c03217f41201d88769457be2
Guangshi Jie <guangshi18@gmail.com>
2023-01-26 14:57:10 -0800

    updated Readme, set up pic file

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,37 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
-// set up time function
-// function setTime() {
-//   var timerInterval = setInterval(function(){
-//     todaysDate = dayjs().format("MMM D, YYYY, hh:mm:ss");
-//     $("#currentDay").text(todaysDate);
-//   }, 1000)
-// }
-
-
-
 
 $(document).ready(function(){
 // set up timer
@@ -51,38 +17,38 @@ $(document).ready(function(){
 })
 
 // save data from local storage for each hour
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-1 .description").val(localStorage.getItem("hour-1"));
-$("#hour-2 .description").val(localStorage.getItem("hour-2"));
-$("#hour-3 .description").val(localStorage.getItem("hour-3"));
-$("#hour-4 .description").val(localStorage.getItem("hour-4"));
-$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 function timeTracker(){
   var currentHour = dayjs().hour();
 
   $(".time-block").each(function(){
-    var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+    var blockHour = parseInt($(this).attr("id"));
     console.log(blockHour, currentHour)
-
+// compare the current time with blocktime
     if(currentHour > blockHour){
-      $(this).addClass("pass");
-      $(this).removeClass("futrue");
+      $(this).addClass("past");
+      $(this).removeClass("future");
       $(this).removeClass("present");
     }
     else if (currentHour === blockHour){
       $(this).addClass("present");
-      $(this).removeClass("futrue");
-      $(this).removeClass("pass");
+      $(this).removeClass("future");
+      $(this).removeClass("past");
     }
     else {
-      $(this).addClass("futrue");
-      $(this).removeClass("pass");
+      $(this).addClass("future");
+      $(this).removeClass("past");
       $(this).removeClass("present");
     }
   })
-  timeTracker();
-}
+};
+timeTracker();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Work-Day-Scheduler
+This is a simple calender application that allows a user to save events for each hour of the day.
+
+User will can schedule events in this application through their day.
+
+User need to click save button to save their events.
+
+Text boxes' colors will change color according to real time.
+
